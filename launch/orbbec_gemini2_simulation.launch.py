7a7457cf86f094e03c0f7dc60a07524af4423199
Martin Volz <mavo-on@gmx.de>
2026-05-06 15:12:59 +0200

    Added camera launch files for a container static transform launch

---
+++ launch/orbbec_gemini2_simulation.launch.py
@@ -0,0 +1,99 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+from tf_transformations import quaternion_from_euler
+from launch.actions import TimerAction
+
+
+def generate_launch_description():
+    ns = LaunchConfiguration("ros_namespace")
+    cam = LaunchConfiguration("camera_name")
+
+    # Define the transforms using list concatenation for substitutions
+    transforms = [
+        {
+            "x": 0.01491,
+            "y": 0.025,
+            "z": 0.0,
+            "roll": 0.0,
+            "pitch": 0.0,
+            "yaw": 0.0,
+            "parent": "_link",
+            "child": "_depth_frame",
+        },
+        {
+            "x": 0.0,
+            "y": 0.0,
+            "z": 0.0,
+            "roll": -1.5708,
+            "pitch": 0.0,
+            "yaw": -1.5708,
+            "parent": "_depth_frame",
+            "child": "_depth_optical_frame",
+        },
+        {
+            "x": 0.01682,
+            "y": -0.010995,
+            "z": 0.0,
+            "roll": 0.0,
+            "pitch": 0.0,
+            "yaw": 0.0,
+            "parent": "_depth_frame",
+            "child": "_color_frame",
+        },
+        {
+            "x": 0.0,
+            "y": 0.0,
+            "z": 0.0,
+            "roll": -1.5708,
+            "pitch": 0.0,
+            "yaw": -1.5708,
+            "parent": "_color_frame",
+            "child": "_color_optical_frame",
+        },
+    ]
+
+    composable_nodes = []
+    for tf in transforms:
+        q = quaternion_from_euler(tf["roll"], tf["pitch"], tf["yaw"])
+
+        composable_nodes.append(
+            ComposableNode(
+                package="tf2_ros",
+                plugin="tf2_ros::StaticTransformBroadcasterNode",
+                name=[cam, tf["parent"], "_to_", cam, tf["child"]],
+                parameters=[
+                    {
+                        "translation.x": tf["x"],
+                        "translation.y": tf["y"],
+                        "translation.z": tf["z"],
+                        "rotation.x": q[0],
+                        "rotation.y": q[1],
+                        "rotation.z": q[2],
+                        "rotation.w": q[3],
+                        "frame_id": [cam, tf["parent"]],
+                        "child_frame_id": [cam, tf["child"]],
+                    }
+                ],
+                remappings=[("/tf_static", "tf_static")],
+            )
+        )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument("ros_namespace", default_value="athena"),
+            DeclareLaunchArgument("camera_name", default_value="orbbec_gemini2"),
+            # Add a delay to ensure the container is ready
+            TimerAction(
+                period=1.0,  # Wait 1 seconds for the container to initialize
+                actions=[
+                    LoadComposableNodes(
+                        target_container=[ns, "/static_tf_publisher_container"],
+                        composable_node_descriptions=composable_nodes,
+                    )
+                ],
+            ),
+        ]
+    )
