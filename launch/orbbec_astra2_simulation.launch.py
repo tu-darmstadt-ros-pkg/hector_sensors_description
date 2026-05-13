6a20cb58a687b80779ae16bf1f0c4604d6a557ec
Martin Volz <57395058+MaddinVo@users.noreply.github.com>
2026-05-13 17:45:34 +0200

    static transform container (#20)
    * Added camera launch files for a container static transform launch
    
    * Added launch files for container launch of static transforms of cameras
    
    * Updated launch files to not use a TimerAction
    
    * Delete .idea/editor.xml
    
    * Delete .idea/hector_sensors_description.iml
    
    * update
    
    * Renamed to orbbec_astra_...
    
    ---------
    
    Co-authored-by: Frederik <bark@sim.tu-darmstadt.de>

---
+++ launch/orbbec_astra2_simulation.launch.py
@@ -0,0 +1,98 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import LoadComposableNodes
+from launch_ros.descriptions import ComposableNode
+from tf_transformations import quaternion_from_euler
+
+# Note that the container needs to be created before the composable node of this launch file can be created.
+
+
+def generate_launch_description():
+    ns = LaunchConfiguration("ros_namespace")
+    cam = LaunchConfiguration("static_container_name")
+    container_name = LaunchConfiguration("static_container_name")
+
+    # Define the transforms using list concatenation for substitutions
+    transforms = [
+        {
+            "x": 0.033,
+            "y": 0.022,
+            "z": 0.0,
+            "roll": 0.0,
+            "pitch": 0.0,
+            "yaw": 0.0,
+            "parent": "_link",
+            "child": "_depth_frame",
+        },
+        {
+            "x": 0.0,
+            "y": 0.0,
+            "z": 0.0,
+            "roll": -1.5708,
+            "pitch": 0.0,
+            "yaw": -1.5708,
+            "parent": "_depth_frame",
+            "child": "_depth_optical_frame",
+        },
+        {
+            "x": 0.033,
+            "y": -0.022,
+            "z": 0.0,
+            "roll": 0.0,
+            "pitch": 0.0,
+            "yaw": 0.0,
+            "parent": "_link",
+            "child": "_color_frame",
+        },
+        {
+            "x": 0.0,
+            "y": 0.0,
+            "z": 0.0,
+            "roll": -1.5708,
+            "pitch": 0.0,
+            "yaw": -1.5708,
+            "parent": "_color_frame",
+            "child": "_color_optical_frame",
+        },
+    ]
+
+    composable_nodes = []
+    for tf in transforms:
+        q = quaternion_from_euler(tf["roll"], tf["pitch"], tf["yaw"])
+
+        composable_nodes.append(
+            ComposableNode(
+                package="tf2_ros",
+                plugin="tf2_ros::StaticTransformBroadcasterNode",
+                name=[cam, tf["parent"], "_to_", cam, tf["child"]],
+                parameters=[
+                    {
+                        "translation.x": tf["x"],
+                        "translation.y": tf["y"],
+                        "translation.z": tf["z"],
+                        "rotation.x": q[0],
+                        "rotation.y": q[1],
+                        "rotation.z": q[2],
+                        "rotation.w": q[3],
+                        "frame_id": [cam, tf["parent"]],
+                        "child_frame_id": [cam, tf["child"]],
+                    }
+                ],
+                remappings=[("/tf_static", "tf_static")],
+            )
+        )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument("ros_namespace", default_value=""),
+            DeclareLaunchArgument("camera_name", default_value="orbbec_astra2"),
+            DeclareLaunchArgument(
+                "static_container_name", default_value="static_tf_publisher_container"
+            ),
+            LoadComposableNodes(
+                target_container=[ns, "/", container_name],
+                composable_node_descriptions=composable_nodes,
+            ),
+        ]
+    )
